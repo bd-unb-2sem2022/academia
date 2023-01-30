8450e568fef741213f71a276fb801e979792cda3
oseias-romeiro <oseias.magalhaes@trf1.jus.br>
2023-01-30 16:24:08 -0300

    iniciado as paginas e as rotas de navegação

--- gymApp/app/views.py
+++ gymApp/app/views.py
@@ -4,3 +4,19 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+class CadastroView(TemplateView):
+    template_name = 'cadastro.html'
+
+class MontagemTreinoView(TemplateView):
+    template_name = 'montagem_treino.html'
+
+class PerfilView(TemplateView):
+    template_name = 'perfil.html'
+
+class ClienteView(TemplateView):
+    template_name = 'cliente.html'
+    
