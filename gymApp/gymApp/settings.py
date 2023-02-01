6f97c68256297b484d2bc70641790e32097f03b0
oseias-romeiro <oseiasromeirom@gmail.com>
2023-01-31 22:20:39 -0300

    sqlite3

--- gymApp/gymApp/settings.py
+++ gymApp/gymApp/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'gymApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-''' TODO: database config
+# TODO: connect to other database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
