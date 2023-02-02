eb7889d39c60de48333dd237843e814624789e36
oseias-romeiro <oseiasromeirom@gmail.com>
2023-02-02 18:21:50 -0300

    loggin

--- gymApp/app/views.py
+++ gymApp/app/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import auth
+from django.shortcuts import redirect
 
-from app.models import Unidade, Plano, Modalidade
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+from app.models import Unidade, Plano, Modalidade, Aluno, Profissional
 
 # Create your views here.
 
@@ -22,18 +27,37 @@ class IndexView(ListView):
 
         return context
 
-class LoginView(TemplateView):
+@method_decorator(csrf_exempt, name='dispatch')
+class LoginView(ListView):
     template_name = 'login.html'
+    model = Profissional
+
+    def post(self, request, *args, **kwargs):
+        cpf = self.request.POST.get('cpf')
+        #auth.login(request)
+        try:
+            Profissional.objects.get(cpf=cpf)
+        except:
+            return redirect('/login/')
+        
+        return redirect('/')
+
+    def get_queryset(self):
+        return super().get_queryset()
+    
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+    
 
 class CadastroView(TemplateView):
     template_name = 'cadastro.html'
 
-class MontagemTreinoView(TemplateView, LoginRequiredMixin):
+class MontagemTreinoView(LoginRequiredMixin, TemplateView):
     template_name = 'montagem_treino.html'
 
-class PerfilView(TemplateView, LoginRequiredMixin):
+class PerfilView(LoginRequiredMixin, TemplateView):
     template_name = 'perfil.html'
 
-class ClienteView(TemplateView, LoginRequiredMixin):
+class ClienteView(LoginRequiredMixin, TemplateView):
     template_name = 'cliente.html'
 
