3ee1e6a314da435dec9e60a852f12829cf7c2b36
oseias-romeiro <oseiasromeirom@gmail.com>
2023-02-03 17:21:31 -0300

    inserção de aluno

--- gymApp/app.py
+++ gymApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 from db import conexao
 
@@ -13,5 +13,44 @@ def index():
     res = cursor.fetchall()
     return render_template('index.html', alunos=res)
 
+@app.route('/add_aluno', methods=['GET', 'POST'])
+def get_add_aluno():
+    if request.method == 'GET':
+        
+        cursor.execute('select codigo, nome, valor from plano')
+        plano = cursor.fetchall()
 
+        cursor.execute('select codigo, fk_modalidade_codigo from turma')
+        turma = cursor.fetchall()
+        
+        return render_template('add_aluno.html', plano=plano, turma=turma)
+
+    elif request.method == 'POST':
+
+        # dados do formulario
+        cpf = request.form.get('cpf')
+        nome = request.form.get('nome')
+        data_nascimento = request.form.get('data_nascimento')
+        foto = request.form.get('foto')
+        print('foto:', foto)
+        fk_plano_codigo = request.form.get('fk_plano_codigo')
+        fk_turma = request.form.get('fk_turma')
+
+        # TODO: validação e sanitização dos dados inseridos
+
+        # insert
+        try:
+            # TODO: adicionar foto
+            cursor.execute(f"insert into aluno values ('{cpf}', '{nome}', '{data_nascimento}', '{foto}', '{fk_plano_codigo}')")
+            conexao.commit()
+
+            cursor.execute(f"insert into esta_matriculado values ('{fk_turma}', '{cpf}')")
+            conexao.commit()
+        except:
+            return 'Erro ao inserir aluno'
+
+        return redirect('/')
+
+    else:
+        return redirect('/')
 
