00d5ded2918ed2308e6d69e773772de48ae6607b
oseias-romeiro <oseiasromeirom@gmail.com>
2023-02-03 21:29:32 -0300

    deletando alunos e menssageria

--- gymApp/app.py
+++ gymApp/app.py
@@ -1,17 +1,25 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from psycopg2 import Binary
 from db import conexao
 
 app = Flask(__name__)
+
+app.secret_key = 'e7b047ff4089f5d0a187d4c44d8633115af223407bd578511c3ba346cf860805'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 cursor = conexao.cursor()
 
 # routes
 @app.route('/')
 def index():
-    cursor.execute('select aluno, turma_codigo, modalidade, unidade_nome from nome_aluno_modalidade_unidade')
+    try:
+        messages = session['messages']
+    except:
+        messages = ''
+
+    cursor.execute('select aluno, turma_codigo, modalidade, unidade_nome, cpf from nome_aluno_modalidade_unidade')
     res = cursor.fetchall()
-    return render_template('index.html', alunos=res)
+    return render_template('index.html', alunos=res, messages=messages)
 
 @app.route('/add_aluno', methods=['GET', 'POST'])
 def get_add_aluno():
@@ -40,15 +48,35 @@ def get_add_aluno():
 
         try:
             cursor.execute("insert into aluno values (%s,%s,%s,%s,%s)", (cpf, nome, data_nascimento, foto, fk_plano_codigo))
+            cursor.execute("insert into esta_matriculado values (%s,%s)", (fk_turma, cpf))
             conexao.commit()
-
-            cursor.execute(f"insert into esta_matriculado values ('{fk_turma}', '{cpf}')")
-            conexao.commit()
+            
+            session['messages'] = 'Aluno inserido com sucesso'
         except:
-            return 'Erro ao inserir aluno'
+            session['messages'] = 'Erro ao inserir aluno'
 
         return redirect('/')
 
     else:
+        session['messages'] = 'verbo HTTP não permitido'
         return redirect('/')
 
+@app.route('/delete_aluno')
+def delete_aluno():
+    cpf = request.args.get('aluno')
+
+    try:
+        cursor.execute(f"delete from esta_matriculado where fk_aluno_cpf='{cpf}'")
+        cursor.execute(f"delete from aluno where cpf='{cpf}'")
+        conexao.commit()
+    except:
+        session['messages'] = 'Erro ao deletar aluno'
+        return redirect('/')
+
+    session['messages'] = 'Aluno deletado com sucesso'
+    return redirect('/')
+    
+@app.route('/clear_session')
+def clear_session():
+    session.clear()
+    return redirect('/')
